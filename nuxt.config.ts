1e00cb28838847a227c4399460d10e0f1b5f937b
Eita <kusaiyuka@gmail.com>
2026-09-20 13:51:21 +0900

    feat(sitemap): サイトマップの設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,9 @@ export default defineNuxtConfig({
     url: 'https://eita.ptmi.jp',
     name: '小林栄太のポートフォリオサイト',
   },
+  sitemap: {
+    strictNuxtContentPaths: true,
+  },
   studio: {
     repository: {
       provider: 'github',
